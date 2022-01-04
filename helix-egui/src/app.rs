b59446f5968c6c59de9b843c31c7078b6ce35d5a
Pearce Keesling <keeslinp@gmail.com>
2022-01-03 18:07:59 -0700

    helix-egui: Manual text layout to handle some weird line wrapping bugs in egui
    Pretty hackish as a first pass, but works surprisingly well

--- helix-egui/src/app.rs
+++ helix-egui/src/app.rs
@@ -1,8 +1,6 @@
-use egui::{Color32, Frame, Layout, Ui, Vec2, Widget};
-use helix_core::{merge_toml_values, syntax};
-use helix_lsp::{lsp, util::lsp_pos_to_pos, LspProgressMap};
+use egui::{Color32, Frame, Layout, Pos2, Ui, Vec2, Widget};
+use helix_core::{merge_toml_values, syntax, Position};
 use helix_view::{editor::Action, graphics::Rect, theme, Document, Editor, View};
-use serde_json::json;
 
 use anyhow::Result;
 
@@ -116,14 +114,18 @@ impl<'a> Widget for ViewWidget<'a> {
             .show(ui, |ui| {
                 ui.allocate_ui(
                     (
-                        self.view.inner_area().width as f32
+                        self.view.area.width as f32
                             * ui.fonts().glyph_width(egui::TextStyle::Monospace, 'm'),
-                        self.view.inner_area().height as f32
+                        self.view.area.height as f32
                             * ui.fonts().row_height(egui::TextStyle::Monospace),
                     )
                         .into(),
                     |ui| {
-                        ui.add(DocumentWidget { doc });
+                        ui.add(DocumentWidget {
+                            doc,
+                            offset: self.view.offset,
+                            area: self.view.inner_area(),
+                        });
                     },
                 );
                 ui.with_layout(Layout::bottom_up(egui::Align::Min), |ui| {
@@ -141,12 +143,101 @@ impl<'a> Widget for ViewWidget<'a> {
 
 struct DocumentWidget<'a> {
     doc: &'a Document,
+    offset: Position,
+    area: Rect,
+}
+
+fn dumb_log(num: usize) -> usize {
+    match num {
+        0..=9 => 1,
+        10..=99 => 2,
+        100..=999 => 3,
+        1000..=9999 => 4,
+        _ => unreachable!(), // TODO: make this not suck :)
+    }
 }
 
 impl<'a> Widget for DocumentWidget<'a> {
     fn ui(self, ui: &mut Ui) -> egui::Response {
-        for chunk in self.doc.text().chunks() {
-            ui.label(chunk);
+        let line_height = ui.fonts().row_height(egui::TextStyle::Monospace);
+        let char_width = ui.fonts().glyph_width(egui::TextStyle::Monospace, 'm');
+        let text = self.doc.text().slice(..);
+        let last_line = std::cmp::min(
+            // Saturating subs to make it inclusive zero indexing.
+            (self.offset.row + self.area.height as usize - 1).saturating_sub(1),
+            self.doc.text().len_lines().saturating_sub(1),
+        );
+
+        let range = {
+            // calculate viewport byte ranges
+            let start = text.line_to_byte(self.offset.row);
+            let end = text.line_to_byte(last_line + 1);
+
+            start..end
+        };
+        let painter = ui.painter();
+        let available_rect = ui.available_rect_before_wrap();
+        let top_left = available_rect.left_top();
+        let mut paint_cursor = top_left;
+        let mut wrapped_lines = 0;
+        'outer: for (index, line) in text.slice(range).lines().enumerate() {
+            let mut remaining_available_chars = self.area.width as usize;
+
+            // Render gutter
+            paint_cursor += Vec2::RIGHT * char_width * (5 - dumb_log(index)) as f32;
+
+            painter.text(
+                paint_cursor,
+                egui::Align2::LEFT_TOP,
+                index.to_string(),
+                egui::TextStyle::Monospace,
+                Color32::WHITE,
+            );
+            paint_cursor += Vec2::RIGHT * char_width * (dumb_log(index) + 1) as f32;
+
+            for chunk in line.chunks() {
+                let mut remaining_opt: Option<&str> = Some(chunk);
+                while let Some(remaining) = remaining_opt {
+                    let (head, rest) = remaining.split_at(
+                        remaining
+                            .char_indices()
+                            .map(|(i, c)| i + c.len_utf8())
+                            .take_while(|i| *i <= remaining_available_chars)
+                            .last()
+                            .unwrap_or(0),
+                    );
+                    let rect = painter.text(
+                        paint_cursor,
+                        egui::Align2::LEFT_TOP,
+                        head,
+                        egui::TextStyle::Monospace,
+                        Color32::WHITE,
+                    );
+                    paint_cursor += Vec2::RIGHT * rect.width();
+                    if rest.len() > 0 {
+                        wrapped_lines += 1;
+                        remaining_available_chars = self.area.width as usize;
+                        paint_cursor = Pos2 {
+                            x: top_left.x + char_width * 6.,
+                            y: paint_cursor.y + line_height,
+                        };
+                        remaining_opt = Some(rest);
+                        if index + wrapped_lines >= self.area.height as usize - 2 {
+                            break 'outer; // short-circuit if we're going to pass the end of the screen
+                        }
+                    } else {
+                        remaining_available_chars -= head.len();
+                        remaining_opt = None;
+                    }
+                }
+            }
+            paint_cursor = Pos2 {
+                x: top_left.x,
+                y: paint_cursor.y + line_height,
+            };
+            if index + wrapped_lines >= self.area.height as usize {
+                break 'outer; // short-circuit if we're going to pass the end of the screen
+            }
         }
         ui.allocate_response(ui.available_size(), egui::Sense::focusable_noninteractive())
     }
